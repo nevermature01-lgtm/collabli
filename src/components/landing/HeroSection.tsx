cfdc0f7ccaf65bb64da45298a36a28969fe5a9f0
Adnan Khan <adnankhan75e@gmail.com>
2026-05-02 08:23:39 +0000

    isko aur neeche karo  (_for element div_)

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -91,7 +91,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
       </div>
 
       {/* Floating Testimonial Card 2 */}
-      <div className="hidden lg:block absolute bottom-[100px] right-[40px] w-[110px] bg-white rounded-xl shadow-[0_8px_20px_rgba(0,0,0,0.06)] p-3 z-20 animate-in fade-in slide-in-from-right-10 duration-1000 delay-300">
+      <div className="hidden lg:block absolute bottom-[40px] right-[40px] w-[110px] bg-white rounded-xl shadow-[0_8px_20px_rgba(0,0,0,0.06)] p-3 z-20 animate-in fade-in slide-in-from-right-10 duration-1000 delay-300">
         <div className="text-[#0ab99d] text-lg font-serif mb-1 leading-none">“</div>
         <p className="text-[10px] text-gray-700 leading-tight font-medium">
           We found the perfect influencer in just a few clicks. Great experience!
