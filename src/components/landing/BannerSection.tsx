041e22fd22bef4c212a7c1a7d4a5c5415734b461
Adnan Khan <adnankhan75e@gmail.com>
2026-05-02 13:08:11 +0000

    remove this  (_for element button_)

--- src/components/landing/BannerSection.tsx
+++ src/components/landing/BannerSection.tsx
@@ -49,11 +49,6 @@ export const BannerSection: React.FC = () => {
                   Start Matching Now
                   <ArrowRight className="ml-2 w-3.5 h-3.5 group-hover:translate-x-1 transition-transform" />
                 </button>
-
-                <button className="mt-4 sm:mt-0 sm:ml-[20px] text-[#0ab99d] font-medium text-[10px] md:text-xs relative group py-1 transition-colors hover:text-[#059669]">
-                  View Success Stories
-                  <span className="absolute bottom-0 left-0 w-0 h-[1.5px] bg-[#0ab99d] transition-all duration-300 group-hover:w-full"></span>
-                </button>
               </div>
             </div>
 
