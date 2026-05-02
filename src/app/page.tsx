255fe41581fa3352918dc66f367f37119dfeb619
Adnan Khan <adnankhan75e@gmail.com>
2026-05-02 08:15:39 +0000

    thoda bada karo bahot chota ho gaya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <img
           src="/logo.png"
           alt="Collabli Logo"
-          className="h-5 md:h-6 lg:h-7 w-auto object-contain select-none"
+          className="h-7 md:h-9 lg:h-10 w-auto object-contain select-none"
         />
       </div>
 
